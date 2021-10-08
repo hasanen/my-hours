62daebe98e61379800c57f88e1103cccfcb9d71c
Joni Hasanen <joni@pieceofcode.com>
2021-10-08 11:40:49 +0300

    Some testing for config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use config;
+use directories_next::BaseDirs;
 use structopt::StructOpt;
 mod hours;
 mod integrations;
@@ -20,9 +22,22 @@ enum Subcommand {
 }
 
 fn main() {
+    let config = load_config();
+    println!("{:?}", config);
+
     let args = Arguments::from_args();
     match &args.subcommand {
         Some(Subcommand::IntegrationsCommand { action }) => integrations::execute(action),
         None => hours::execute(),
     }
+}
+
+fn load_config() -> config::Config {
+    if let Some(base_dirs) = BaseDirs::new() {
+        println!("{}", base_dirs.config_dir().display());
+        // Lin: /home/alice/.config/barapp
+        // Win: C:\Users\Alice\AppData\Roaming\Foo Corp\Bar App\config
+        // Mac: /Users/Alice/Library/Application Support/com.Foo-Corp.Bar-App
+    }
+    return config::Config::default();
 }
