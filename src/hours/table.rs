a2dfd1b99ce4f51239381339940bf850a0ff640f
Joni Hasanen <joni@pieceofcode.com>
2021-11-08 14:17:37 +0200

    Add some ideas and break the build

--- src/hours/table.rs
+++ src/hours/table.rs
@@ -5,21 +5,21 @@ use prettytable::{format, Attr, Cell, Row, Table};
 /// Prints given entries to terminal
 pub fn print(time_entries: &types::TimeEntries, common_hours: &types::CommonHours) {
     println!("");
-    print_hours_table(time_entries);
+    print_hours_table(time_entries, common_hours);
     println!("");
     println!("");
     print_common_table(common_hours);
 }
 
-fn print_hours_table(time_entries: &types::TimeEntries) {
+fn print_hours_table(time_entries: &types::TimeEntries, common_hours: &types::CommonHours) {
     let mut table = Table::new();
     table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
     table.set_titles(Row::new(vec![
         header_cell(&"Project"),
         header_cell(&"Today"),
-        header_cell(&"Daily AVG"),
-        header_cell(&"This month"),
-        header_cell(&"Target"),
+        header_cell(&"Current week (Daily AVG)"),
+        header_cell(&"Current month (Daily AVG)"),
+        header_cell(&"Target (day / week  / month)"),
     ]));
     for project in time_entries.uniq_projects() {
         table.add_row(Row::new(vec![
@@ -31,13 +31,19 @@ fn print_hours_table(time_entries: &types::TimeEntries) {
         ]));
     }
     table.add_row(Row::new(vec![
-        Cell::new("Total").style_spec("bFg"),
+        Cell::new("Total").style_spec("b"),
         Cell::new(&format_duration(
             &time_entries.total_hours_for_current_day(),
         ))
-        .style_spec("bFg"),
+        .style_spec(&format!(
+            "b{}",
+            target_hours_color(
+                &(*common_hours.target_daily_hours as i64),
+                &time_entries.total_hours_for_current_day()
+            )
+        )),
         Cell::new(""),
-        Cell::new(&format_duration(&time_entries.total_hours())).style_spec("bFg"),
+        Cell::new(&format_duration(&time_entries.total_hours())).style_spec("b"),
         Cell::new(""),
     ]));
     table.printstd();
@@ -59,10 +65,6 @@ fn print_common_table(common_hours: &types::CommonHours) {
         .style_spec("r"),
     ]));
     table.add_row(Row::new(vec![
-        header_cell(&"Target AVG / day"),
-        Cell::new(&format!("{}h", common_hours.target_daily_hours.to_string())).style_spec("r"),
-    ]));
-    table.add_row(Row::new(vec![
         header_cell(&"Hours left"),
         Cell::new(&format_duration(&common_hours.hours_left())).style_spec("r"),
     ]));
@@ -78,5 +80,14 @@ fn format_duration(duration: &chrono::Duration) -> String {
         format!("{:3}h {:2}m", hours, minutes)
     } else {
         "".to_string()
+    }
+}
+fn target_hours_color(target_daily_hours: &i64, duration: &chrono::Duration) -> String {
+    if target_daily_hours - 1 > duration.num_hours() {
+        "Fr".to_string()
+    } else if target_daily_hours < &duration.num_hours() {
+        "Fg".to_string()
+    } else {
+        "Fy".to_string()
     }
 }
