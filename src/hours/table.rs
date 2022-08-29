17c4e0dd5baa045a26e63038667f4708b78babc2
Joni Hasanen <joni@pieceofcode.com>
2022-08-29 22:47:07 +0300

    Add first test for output

--- src/hours/table.rs
+++ src/hours/table.rs
@@ -7,6 +7,12 @@ use comfy_table::{presets::ASCII_NO_BORDERS, Attribute, Cell, Color, Table};
 /// Prints given entries to terminal
 pub fn print(time_entries: &types::TimeEntries, project_configs: &ProjectConfigs) {
     println!();
+
+    let table = generate_table(&time_entries, &project_configs);
+    println!("{table}");
+}
+
+fn generate_table(time_entries: &types::TimeEntries, project_configs: &ProjectConfigs) -> Table {
     let mut table = Table::new();
     table.load_preset(ASCII_NO_BORDERS).set_header(vec![
         header_cell("Project"),
@@ -42,7 +48,7 @@ pub fn print(time_entries: &types::TimeEntries, project_configs: &ProjectConfigs
         Cell::new(""),
     ]);
 
-    println!("{table}");
+    return table;
 }
 
 fn header_cell<T: ToString>(content: T) -> Cell {
@@ -133,122 +139,179 @@ fn format_target_hour(target: Option<u8>) -> String {
 
 #[cfg(test)]
 mod tests {
-    use chrono::Local;
 
-    use super::*;
+    mod colors {
+        use super::super::*;
 
-    #[test]
-    fn it_gives_hours_color() {
-        assert_eq!(target_hours_color(&None, &Duration::hours(0)), Color::Reset);
-        assert_eq!(
-            target_hours_color(&Some(0), &Duration::hours(0)),
-            Color::Green
-        );
-        assert_eq!(
-            target_hours_color(&Some(1), &Duration::hours(3)),
-            Color::Green
-        );
-        assert_eq!(
-            target_hours_color(&Some(3), &Duration::hours(1)),
-            Color::Red
-        );
-        assert_eq!(
-            target_hours_color(&Some(2), &Duration::hours(1)),
-            Color::Yellow
-        );
+        #[test]
+        fn it_gives_hours_color() {
+            assert_eq!(target_hours_color(&None, &Duration::hours(0)), Color::Reset);
+            assert_eq!(
+                target_hours_color(&Some(0), &Duration::hours(0)),
+                Color::Green
+            );
+            assert_eq!(
+                target_hours_color(&Some(1), &Duration::hours(3)),
+                Color::Green
+            );
+            assert_eq!(
+                target_hours_color(&Some(3), &Duration::hours(1)),
+                Color::Red
+            );
+            assert_eq!(
+                target_hours_color(&Some(2), &Duration::hours(1)),
+                Color::Yellow
+            );
+        }
     }
 
-    #[test]
-    fn it_formats_weekly_monthly_hours() {
-        let project = types::Project {
-            client: None,
-            title: "my-hours".to_string(),
-            key: "".to_string(),
-            entries: vec![],
-        };
-        assert_eq!(format_monthly_hours(&project), "");
-        assert_eq!(format_weekly_hours(&project), "");
+    mod formats {
+        use super::super::*;
+        use chrono::Local;
+        #[test]
+        fn it_formats_weekly_monthly_hours() {
+            let project = types::Project {
+                client: None,
+                title: "my-hours".to_string(),
+                key: "".to_string(),
+                entries: vec![],
+            };
+            assert_eq!(format_monthly_hours(&project), "");
+            assert_eq!(format_weekly_hours(&project), "");
 
-        let mut project = types::Project {
-            client: None,
-            title: "my-hours".to_string(),
-            key: "".to_string(),
-            entries: vec![],
-        };
+            let mut project = types::Project {
+                client: None,
+                title: "my-hours".to_string(),
+                key: "".to_string(),
+                entries: vec![],
+            };
 
-        let today = Local::today();
-        let start = today.and_hms_milli(1, 0, 00, 0);
-        let end = today.and_hms_milli(13, 0, 0, 0);
+            let today = Local::today();
+            let start = today.and_hms_milli(1, 0, 00, 0);
+            let end = today.and_hms_milli(13, 0, 0, 0);
 
-        project.entries.push(types::TimeEntry {
-            description: "Monday".to_string(),
-            client: None,
-            project: "my-hours".to_string(),
-            start: Some(start),
-            end: Some(end),
-            billable_amount_cents: 1,
-        });
+            project.entries.push(types::TimeEntry {
+                description: "Monday".to_string(),
+                client: None,
+                project: "my-hours".to_string(),
+                start: Some(start),
+                end: Some(end),
+                billable_amount_cents: 1,
+            });
 
-        assert_eq!(format_monthly_hours(&project), " 12h  0m /  12h  0m");
-        assert_eq!(format_weekly_hours(&project), " 12h  0m /  12h  0m");
+            assert_eq!(format_monthly_hours(&project), " 12h  0m /  12h  0m");
+            assert_eq!(format_weekly_hours(&project), " 12h  0m /  12h  0m");
+        }
+
+        #[test]
+        fn it_formats_hours() {
+            assert_eq!(
+                format_hours(&Duration::hours(1), &Duration::hours(2)),
+                "  1h  0m /   2h  0m"
+            );
+            assert_eq!(format_hours(&Duration::hours(0), &Duration::hours(2)), "");
+            assert_eq!(
+                format_hours(&Duration::hours(1), &Duration::hours(0)),
+                "  1h  0m / "
+            );
+        }
+
+        #[test]
+        fn it_formats_project_title() {
+            let project_no_client = types::Project {
+                client: None,
+                title: "my-hours".to_string(),
+                key: "".to_string(),
+                entries: vec![],
+            };
+            assert_eq!(format_project_title(&project_no_client), "my-hours");
+
+            let project_with_client = types::Project {
+                client: Some("hasanen".to_string()),
+                title: "my-hours".to_string(),
+                key: "".to_string(),
+                entries: vec![],
+            };
+            assert_eq!(
+                format_project_title(&project_with_client),
+                "hasanen / my-hours"
+            );
+        }
+
+        #[test]
+        fn it_formats_targets() {
+            let config = ProjectConfig {
+                target_daily_hours: Some(1),
+                target_weekly_hours: Some(2),
+                target_monthly_hours: Some(3),
+            };
+
+            assert_eq!(format_targets(&config), "1h / 2h / 3h");
+
+            let config_without_targets = ProjectConfig {
+                target_daily_hours: None,
+                target_weekly_hours: None,
+                target_monthly_hours: None,
+            };
+            assert_eq!(format_targets(&config_without_targets), "");
+        }
+
+        #[test]
+        fn it_formats_target_hours() {
+            assert_eq!(format_target_hour(Some(1)), "1h");
+            assert_eq!(format_target_hour(None), "-");
+        }
     }
 
-    #[test]
-    fn it_formats_hours() {
-        assert_eq!(
-            format_hours(&Duration::hours(1), &Duration::hours(2)),
-            "  1h  0m /   2h  0m"
-        );
-        assert_eq!(format_hours(&Duration::hours(0), &Duration::hours(2)), "");
-        assert_eq!(
-            format_hours(&Duration::hours(1), &Duration::hours(0)),
-            "  1h  0m / "
-        );
-    }
+    mod formats_table {
+        use super::super::*;
+        use chrono::Local;
+        use digest::Digest;
+        use sha2::Sha256;
+        use std::collections::HashMap;
 
-    #[test]
-    fn it_formats_project_title() {
-        let project_no_client = types::Project {
-            client: None,
-            title: "my-hours".to_string(),
-            key: "".to_string(),
-            entries: vec![],
-        };
-        assert_eq!(format_project_title(&project_no_client), "my-hours");
+        #[test]
+        fn formats_table_with_one_project_work_done_current_day_no_targets() {
+            let project_name = "Project".to_string();
+            let time_entries = types::TimeEntries {
+                entries: [types::TimeEntry {
+                    description: "Description".to_string(),
+                    client: None,
+                    project: project_name.clone(),
+                    billable_amount_cents: 0,
+                    start: Some(Local::now()),
+                    end: Some(
+                        Local::now()
+                            .checked_add_signed(Duration::minutes(60))
+                            .unwrap(),
+                    ),
+                }]
+                .to_vec(),
+            };
+            let mut hasher = Sha256::default();
+            hasher.update(&project_name);
+            let project_key = format!("{:x}", &hasher.finalize());
 
-        let project_with_client = types::Project {
-            client: Some("hasanen".to_string()),
-            title: "my-hours".to_string(),
-            key: "".to_string(),
-            entries: vec![],
-        };
-        assert_eq!(
-            format_project_title(&project_with_client),
-            "hasanen / my-hours"
-        );
-    }
+            let project_configs = ProjectConfigs {
+                configs: HashMap::from([(
+                    project_key,
+                    ProjectConfig {
+                        target_daily_hours: None,
+                        target_weekly_hours: None,
+                        target_monthly_hours: None,
+                    },
+                )]),
+            };
+            let mut hours_table = generate_table(&time_entries, &project_configs);
+            hours_table.force_no_tty();
+            let expected = "
+ Project | Today    | Current week / Daily AVG | Current month / Daily AVG | Target (day / week / month) 
+=========================================================================================================
+ Project |   1h  0m |   1h  0m /   1h  0m      |   1h  0m /   1h  0m       |                             
+---------+----------+--------------------------+---------------------------+-----------------------------
+ Total   |   1h  0m |                          |   1h  0m                  |                             ";
 
-    #[test]
-    fn it_formats_targets() {
-        let config = ProjectConfig {
-            target_daily_hours: Some(1),
-            target_weekly_hours: Some(2),
-            target_monthly_hours: Some(3),
-        };
-
-        assert_eq!(format_targets(&config), "1h / 2h / 3h");
-
-        let config_without_targets = ProjectConfig {
-            target_daily_hours: None,
-            target_weekly_hours: None,
-            target_monthly_hours: None,
-        };
-        assert_eq!(format_targets(&config_without_targets), "");
-    }
-
-    #[test]
-    fn it_formats_target_hours() {
-        assert_eq!(format_target_hour(Some(1)), "1h");
-        assert_eq!(format_target_hour(None), "-");
+            assert_eq!("\n".to_string() + &hours_table.to_string(), expected)
+        }
     }
 }
