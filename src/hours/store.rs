6296b92833340f10a2bae3b590df5348bf184a68
Joni Hasanen <joni@pieceofcode.com>
2023-10-05 21:15:25 +0300

    Finish implementation for DiskStore

--- src/hours/store.rs
+++ src/hours/store.rs
@@ -1,28 +1,26 @@
-//! File storage module.
+//! Storage module
 use crate::hours::types;
-use crate::settings;
 use crate::string_types::FilePath;
 use std::fs;
 
-
+/// Common store functionality
 pub trait Store {
     fn save(&self, time_entries: &types::TimeEntries) -> Result<(), std::io::Error>;
     fn load(&self) -> types::TimeEntries;
 }
+/// Filesystem storage for hours
 pub struct DiskStore {
+    /// Path to the file where hours are stored in filesystem
     pub path: FilePath,
 }
 
 impl Store for DiskStore {
     fn save(&self, time_entries: &types::TimeEntries) -> Result<(), std::io::Error> {
-        // let monthly_hours_path = settings::app_path(HOURS_FILENAME)
-        //     .unwrap_or_else(|| panic!("Failed to locate {}", HOURS_FILENAME));
-        // let toml = toml::to_string(&time_entries).unwrap();
-        // fs::write(monthly_hours_path, toml)
-        Ok(())
+        let toml = toml::to_string(&time_entries).unwrap();
+        fs::write(self.path.as_str(), toml)
     }
 
-        fn load(&self) -> types::TimeEntries {
+    fn load(&self) -> types::TimeEntries {
         let hours_str = fs::read_to_string(self.path.as_str()).expect("Couldn't load hours");
         if hours_str.trim().is_empty() {
             types::TimeEntries {
@@ -34,39 +32,29 @@ impl Store for DiskStore {
     }
 }
 
-// /// Store time entries to file on disk
-// pub fn save(time_entries: &types::TimeEntries) -> Result<(), std::io::Error> {
-//     let monthly_hours_path = settings::app_path(HOURS_FILENAME)
-//         .unwrap_or_else(|| panic!("Failed to locate {}", HOURS_FILENAME));
-//     let toml = toml::to_string(&time_entries).unwrap();
-//     fs::write(monthly_hours_path, toml)
-// }
-
-// /// Load time entries from file on disk
-// pub fn load() -> types::TimeEntries {
-//     let monthly_hours_path = settings::app_path(HOURS_FILENAME)
-//         .unwrap_or_else(|| panic!("Failed to locate {}", HOURS_FILENAME));
-//     let hours_str = fs::read_to_string(monthly_hours_path).expect("Couldn't load settings");
-//     if hours_str.trim().is_empty() {
-//         types::TimeEntries {
-//             entries: Vec::new(),
-//         }
-//     } else {
-//         toml::from_str(&hours_str).unwrap()
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use super::super::*;
-    use crate::string_types::{FilePath, Description, ProjectName};
+    use crate::string_types::{Description, FilePath, ProjectName};
     use chrono::Duration;
     use std::fs::File;
 
     #[test]
+    fn return_empty_list_of_entries_by_default() {
+        let filepath = FilePath("/tmp/test.toml".to_string());
+        File::create(&filepath.as_str()).unwrap();
+        let store = DiskStore {
+            path: FilePath("/tmp/test.toml".to_string()),
+        };
+
+        let loaded_entries = store.load();
+
+        assert_eq!(loaded_entries.entries.len(), 0);
+    }
+    #[test]
     fn saves_entries_into_file() {
         let filepath = FilePath("/tmp/test.toml".to_string());
-        File::create(&filepath.as_str())?;
+        File::create(&filepath.as_str()).unwrap();
         let store = DiskStore {
             path: FilePath("/tmp/test.toml".to_string()),
         };
@@ -83,7 +71,7 @@ mod tests {
                         .unwrap(),
                 ),
             }]
-            .to_vec()
+            .to_vec(),
         };
         store.save(&time_entries).unwrap();
         let loaded_entries = store.load();
